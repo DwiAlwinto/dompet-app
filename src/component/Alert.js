9ae6a366e3068ebffe640db03c3d8ad6c7c6259f
unknown <dwi.alwinto@gmail.com>
2023-07-20 23:44:51 +0700

    first commit

---
+++ src/component/Alert.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Alert extends React.Component{
+    constructor(){
+      super();
+    }
+  
+    render(){
+      return(
+        <h1>Data Masih Kosong</h1>
+      )
+    }
+  }
+
+  export default Alert;
